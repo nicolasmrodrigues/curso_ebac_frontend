34ba67e7225b549b3db1eb7eb5e8b10ce15bb897
Nícolas Medeiros Rodrigues <nicolasmedeiros090@gmail.com>
2023-06-16 11:12:26 -0300

    removendo espaço antes do ;

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 const mutiplicar = (x: number, y: number): number => x * y;
 
-const dizOla = (nome: string): string => `Olá ${nome}` ;
+const dizOla = (nome: string): string => `Olá ${nome}`;
 
 let teste1 = mutiplicar(10, 2);
 let teste2 = dizOla('Nícolas');
