5412d655fdedc4088f84e9b3f990b5a11459daef
Nícolas Medeiros Rodrigues <nicolasmedeiros090@gmail.com>
2023-06-16 10:25:03 -0300

    adicionando exercicio_ts

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-const boletim = [
-    {nome: 'Nicolas', nota: 8},
-    {nome: 'Gabriel', nota: 9},
-    {nome: 'Caio', nota: 5},
-    {nome: 'Ricardo', nota: 4},
-    {nome: 'Lucas', nota: 6},
-    {nome: 'Gustavo', nota: 3},
-    {nome: 'Fernando', nota: 10}
-]
-
-const aprovados = boletim.filter(aluno => aluno.nota >= 6);
-
-console.log(aprovados);
+"use strict";
+const mutiplicar = (x, y) => x * y;
+const dizOla = (nome) => `Olá ${nome}`;
+let teste1 = mutiplicar(10, 2);
+let teste2 = dizOla('Nícolas');
+console.log(teste1);
+console.log(teste2);
---
+++ main.ts
@@ -0,0 +1,9 @@
+const mutiplicar = (x: number, y: number): number => x * y;
+
+const dizOla = (nome: string): string => `Olá ${nome}` ;
+
+let teste1 = mutiplicar(10, 2);
+let teste2 = dizOla('Nícolas');
+
+console.log(teste1);
+console.log(teste2);
